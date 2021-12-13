85930e74e1975e69611d1a3dbbfed8d0acf45e80
sergiogval <sergioval@gmail.com>
2021-12-13 15:21:34 -0600

    Add functionality to show mobile menu

--- main.js
+++ main.js
@@ -2,5 +2,5 @@ const ham = document.querySelector('.ham');
 const links = document.querySelector('.menu');
 
 ham.addEventListener('click', () => {
-  links.classList.toggle('open');
+  links.style.display = 'block';
 });
