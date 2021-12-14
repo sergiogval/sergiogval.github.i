3158dce4c51733e066a0cd0abf147984998f2192
sergiogval <sergioval@gmail.com>
2021-12-14 15:43:56 -0600

    Add object constructor

---
+++ constructor.js
@@ -0,0 +1,10 @@
+// make an object constructor for an object with the name Projects and the properties id, name, description, image and languages array.
+
+function Projects(id, name, description, image, languages) {
+  this.id = id;
+  this.name = name;
+  this.description = description;
+  this.image = image;
+  this.languages = languages;
+}
+
