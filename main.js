410c6407d0f498d52dd3722c6a3cbea124e7a4f9
sergiogval <sergioval@gmail.com>
2021-12-14 07:44:55 -0600

    Add images and transitions to mobile menu

--- main.js
+++ main.js
@@ -1,10 +1,16 @@
 const ham = document.querySelector('.ham');
 const menu = document.querySelector('.menu');
 const close = document.querySelectorAll('.ham span');
+const logoclosed = document.querySelector('.logo-mobile1');
+const logoclosed1 = document.querySelector('.logo-mobile2');
 
 ham.addEventListener('click', () => {
-  menu.classList.toggle('activate');
+  menu.classList.toggle('active');
+  logoclosed.classList.toggle('active');
+  logoclosed1.classList.toggle('active');
   close.forEach((child) => {
     child.classList.toggle('animated');
   });
 });
+
+// create an array of objects with the data of the project in the index.html file.
