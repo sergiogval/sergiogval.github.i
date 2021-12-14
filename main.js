4c302ecd6e44b54a70c9d6f59c4c34dd0e5143ba
Fernando Herrera <fherrera0206@gmail.com>
2021-12-14 10:22:44 -0500

    Fix the mobile image background

--- main.js
+++ main.js
@@ -3,11 +3,13 @@ const menu = document.querySelector('.menu');
 const close = document.querySelectorAll('.ham span');
 const logoclosed = document.querySelector('.logo-mobile1');
 const logoclosed1 = document.querySelector('.logo-mobile2');
+const mobileImage = document.querySelector('.projects');
 
 ham.addEventListener('click', () => {
   menu.classList.toggle('active');
   logoclosed.classList.toggle('active');
   logoclosed1.classList.toggle('active');
+  mobileImage.style.backgroundImage = "url('images/mobile-menu-img.png')";
   close.forEach((child) => {
     child.classList.toggle('animated');
   });
