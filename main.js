a17e12c524e579ec95c5498d7890683b814d40ee
sergiogval <sergioval@gmail.com>
2021-12-13 20:22:41 -0600

    Fix eslint error

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const close = document.querySelectorAll('.ham span');
 
 ham.addEventListener('click', () => {
   menu.classList.toggle('activate');
-  close.forEach(child => {
+  close.forEach((child) => {
     child.classList.toggle('animated');
   });
-}); 
+});
