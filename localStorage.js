e4760ecb7c98eadd7fa9b221b32d7ca4ebef09f1
sergiogval <sergioval@gmail.com>
2021-12-16 14:38:09 -0600

    Add code block getLocalStorage

--- localStorage.js
+++ localStorage.js
@@ -3,15 +3,27 @@ const emails = document.getElementById('#email');
 const message = document.getElementById('#message');
 const submit = document.getElementById('#submit');
 
+function setLocalStorage() {
+  const data = {
+    name: nameI.value,
+    email: emails.value,
+    message: message.value,
+  };
+  localStorage.setItem('data', JSON.stringify(data));
+}
 submit.addEventListener('click', () => {
-    setLocalStorage()
+  if (null) {
+    setLocalStorage();
+  }
+},
+
+() => {
+  if (localStorage.getItem('data')) {
+    const data = JSON.parse(localStorage.getItem('data'));
+    nameI.value = data.name;
+    emails.value = data.email;
+    message.value = data.message;
+  }
 });
 
-function setLocalStorage() {
-    let data = {
-        name: nameI.value,
-        email: emails.value,
-        message: message.value
-    }
-    localStorage.setItem('data', JSON.stringify(data));
-}
+window.addEventListener('load', getLocalStorage());
