fd3718e11377a44c3388a839530d67f461c1e692
sergiogval <sergioval@gmail.com>
2021-12-18 11:01:14 -0600

    Debug js script

--- constructor.js
+++ constructor.js
@@ -1,16 +1,17 @@
 // object constructor function
-const Projects = function (name, description, image, languages, demo, source) {
+const Projects = function (id, name, description, image, languages, demo, source) {
+  this.id = id;
   this.name = name;
   this.description = description;
   this.image = image;
   this.languages = languages;
   this.demo = demo;
   this.source = source;
-}
+};
 
 // objects populated
 const project01 = new Projects(1, 'Empezando a programar', 'This project is about some tests I did with no idea about what I was doing', '/images/project1.png', ['HTML', 'CSS', 'JS'], 'demo', 'source');
-const project02 = new Projects(2, 'Alien invasion but with a twist', 'A wonderful remake of the classic Alien Invasion but this time the enemy is COVID-19', 'src="/images/project2.png"', ['HTML', 'CSS', 'JS'], 'demo', 'source');
+const project02 = new Projects(2, 'Alien invasion but with a twist', 'A wonderful remake of the classic Alien Invasion but this time the enemy is COVID-19', 'src="images/project2.png"', ['HTML', 'CSS', 'JS'], 'demo', 'source');
 const project03 = new Projects(3, 'A Blog about hemp', 'A personal project that has as ultimate propourse inform people about what hemp is and the 50k derivated products that are available', 'src="/images/project3.png"', ['HTML', 'CSS', 'JS'], 'demo', 'source');
 const project04 = new Projects(4, 'A Generic form page', 'This is a test I did in order to get into the Microverse program. It is just a simple form full of inputs', 'src="/images/project4.png"', ['HTML', 'CSS', 'JS'], 'demo', 'source');
 const project05 = new Projects(5, 'An submmit page', 'This is a project that I did as final block of the module 1 in MV program', 'src="/images/project5.png"', ['HTML', 'CSS', 'JS'], 'demo', 'source');
@@ -20,23 +21,45 @@ const project06 = new Projects(6, 'This project', 'This is my portfolio and it i
 const myProjects = [project01, project02, project03, project04, project05, project06];
 
 const arr = myProjects.length;
+
 const modalTitle = document.querySelector('#modaltitle');
-const modalDescription = document.querySelector('#modalDescription${i}');
-const modalImage = document.querySelector(`modalImage${i}`);
-const modalLanguages = document.querySelector(`modalLanguages${i}`);
-const modalDemo = document.querySelector(`modalDemo${i}`);
-const modalSource = document.querySelector(`modalSource${i}`);
+const modalTemplate = document.querySelector('#modalTemplate');
+const modalDescription = document.querySelector('#modalDescription');
+const modalImage = document.querySelector('#modalImage');
+const modalLanguages = document.getElementById('modalLanguages').children;
+const modalDemo = document.querySelector('#modalDemo');
+const modalSource = document.querySelector('#modalSource');
+const openModals = document.querySelectorAll('.bnt2');
+const modalCloser = document.querySelector('#modalClose');
+const badges = modalLanguages.lenght;
+for (let i = 0; i < badges; i++) {
+  modalLanguages[i].innerText = myProjects[i].languages;
+}
 
 function populateModals() {
+  modalTitle.innerText = myProjects[i].name;
+  modalDescription.innerText = myProjects[i].description;
+  modalImage.src = myProjects[i].image;
+  modalLanguages.innerText = myProjects[i].languages;
+  modalDemo.innerText = myProjects[i].demo;
+  modalSource.innerText = myProjects[i].source;
   for (let i = 0; i <= arr; i++) {
-    modalTitle.innerHTML = myProjects[i].name;
-    modalDescription.innerHTML = myProjects[i].description;
-    modalImage.src = myProjects[i].image;
-    modalLanguages.innerHTML = myProjects[i].languages;
-    modalDemo.innerHTML = myProjects[i].demo;
-    modalSource.innerHTML = myProjects[i].source;
-  };
+  }
 }
-populateModals();
+
+function openModals() {
+addEventListener('click', () => {
+  // for (let i = 0; i <= arr; i++) {
+  modalTemplate.style.display = 'block';
+  populateModals();
+});
+
+// function openModal() {
+//   const modal = document.querySelector('#modal');
+//   modal.style.display = 'block';
+// }
 
 // Create a function closes the modal when the user clicks on the close button
+modalCloser.addEventListener('click', () => {
+  modalTemplate.style.display = 'none';
+})
